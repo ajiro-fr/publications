7dc967926c40c339bf99ef048cd58f86a88c2974
Olivier Albiez <olivier.albiez@poloper.org>
2016-10-15 21:03:27 +0200

    Update delegation poker

--- planning_pocker/deck.rb
+++ planning_pocker/deck.rb
@@ -1,16 +1,27 @@
 require 'squib'
 require 'yaml'
 
-values = Dir["images/*.svg"]
 
-Squib::Deck.new(cards: values.size, layout: 'layout/card.yml') do
-  background color: 'white'
-  rect layout: 'cut'
-  rect layout: 'safe'
-  circle layout: 'topleft_circle'
-  rect layout: 'highlight'
-  svg file: values, layout: 'topleft_value'
-  svg file: values, layout: 'center_value'
-  text str: 'CC 2.0 BY SA Ajiro, http://ajiro.fr', layout: 'copyright'
-  save format: :pdf, file: 'poker.pdf'
+Dir["data/*.yml"].each do |data|
+  puts "processing #{data}"
+  style = File.basename(data, '.yml')
+  content = YAML.load_file(data)
+
+  Squib::Deck.new(cards: content['text'].size, layout: 'layout/card.yml') do
+    background color: 'white'
+    rect layout: 'cut'
+    rect layout: 'outer'
+    rect layout: 'inner'
+    #circle layout: 'topleft_circle'
+    #rect layout: 'highlight'
+
+    text str: content['text'], layout: 'center_value'
+    #text str: content['text'], layout: 'topleft_value'
+
+    #svg file: values, layout: 'topleft_value'
+    #svg file: values, layout: 'center_value'
+    #svg file: "images/break.svg", layout: 'center_value'
+    text str: 'CC 2.0 BY SA Ajiro, http://ajiro.fr', layout: 'copyright'
+    save format: :pdf, file: "poker-#{style}.pdf"
+  end
 end

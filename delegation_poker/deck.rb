7eb810659e2054c56eeeba6058f2fb2bf37484fd
Olivier Albiez <olivier.albiez@poloper.org>
2016-10-19 20:06:42 +0200

    Add illustration version on delegation poker

--- delegation_poker/deck.rb
+++ delegation_poker/deck.rb
@@ -17,6 +17,21 @@ Squib::Deck.new(cards: fr.size, layout: 'layout/card.yml') do
   save format: :pdf, file: "delegation-poker-fr.pdf"
 end
 
+Squib::Deck.new(cards: fr.size, layout: 'layout/card.yml') do
+  background color: fr.map {|i| i['background'] }
+
+  rect layout: 'cut'
+  rect layout: 'safe'
+
+  text str: fr.map {|i| i['number'] }, layout: 'number'
+  text str: fr.map {|i| i['title'].upcase }, layout: 'title'
+  text str: fr.map {|i| i['description'] }, layout: 'description'
+  svg file: fr.map {|i| i['icon'] }, layout: 'icon'
+
+  text str: 'CC 2.0 BY SA Ajiro, http://ajiro.fr', layout: 'copyright'
+  save format: :pdf, file: "delegation-poker-picture-fr.pdf"
+end
+
 Squib::Deck.new(cards: fr.size, layout: 'layout/biface.yml') do
   background color: fr.map {|i| i['background'] }
 
